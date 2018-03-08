4a13bde6f6cf73997fe1e5654cd804a78f7e82ba
Sean Walsh <seanmatthewwalsh@gmail.com>
2018-03-08 12:01:33 +0200

    setup

---
+++ nya-bootstrap4-select/app.js
@@ -0,0 +1,7 @@
+﻿"use strict";
+
+angular.module("app", ["nya.bootstrap.select"])
+    .controller("MainCtrl", function ($scope) {
+
+        $scope.example1options = [{ id: 1, label: "One" }, { id: 2, label: "Two" }, { id: 3, label: "Three" }];
+    });
